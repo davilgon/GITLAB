aeba13bfbf510924c10d1416825f0ad6fe472047
nubbneung <nubb.1@hotmail.com>
2015-04-27 01:38:59 +0700

    Update Program1

--- Program1.c
+++ Program1.c
@@ -12,7 +12,7 @@ int main()
 
     do
     {
-        printf("\n\nEnter number 2-19 (0 to QUIT) : ");
+        printf("\n\nEnter number 2-29 (0 to QUIT) : ");
         scanf("%d",&number);
 
         if (number>=2 && number<=19) //2-19 case
@@ -28,7 +28,7 @@ int main()
                 printf("%5d",i*number);
             }
         }
-        else if (number==0) printf("Have a nice day!!!"); //QUIT case
+        else if (number==0) printf("Goodluck!!!"); //QUIT case
         else printf("Input Error. Enter 0 to QUIT or number between 2-19."); //Error case
 
     }
